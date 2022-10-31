a6caa8f3567ccbd3d7bc11b25e5a65bd2f612304
RahulGopathi <gopathi.1@iitj.ac.in>
2022-11-01 03:32:48 +0530

    Add JWT login with LDAP

--- backend/sso_iitj/urls.py
+++ backend/sso_iitj/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from .views import LoginAndGetUserData
+from .views import MyTokenObtainPairView
 from .swagger import swagger_schema_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('login/', LoginAndGetUserData.as_view(), name='login'),
+    path('login/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('redoc/', swagger_schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('swagger/', swagger_schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
