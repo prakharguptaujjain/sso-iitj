3ed30ebf1fbfa3859ae92fda9fe5bb834d313713
PanyalaSainathReddy <reddy.17@iitj.ac.in>
2022-11-01 03:40:38 +0530

    Adds user_profiles model

--- backend/sso_iitj/urls.py
+++ backend/sso_iitj/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path
 from .views import LoginAndGetUserData
 from .swagger import swagger_schema_view
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,3 +11,6 @@ urlpatterns = [
     path('redoc/', swagger_schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('swagger/', swagger_schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
+
+if settings.DEBUG:
+    urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
