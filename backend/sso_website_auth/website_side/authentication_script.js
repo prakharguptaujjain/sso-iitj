85f772cd239fd6f518c6890a180b68cadff6de7d
PrakharAIP GSOC <emailkakyakarega>
2023-05-02 00:39:01 +0530

    Added sso and website endpoints

--- backend/sso_website_auth/website_side/authentication_script.js
+++ backend/sso_website_auth/website_side/authentication_script.js
@@ -2,8 +2,9 @@ function verifyUser(token, callback) {
     $.ajax({
       url: 'http://auth-server.com/verify',
       data: {
-        site_token: token,
-        user_token: token_from_cookie
+        website_token: token,
+        user_token: token_from_cookie,
+        username : username_from_cookie
       },
     });
   }
